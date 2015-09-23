215946b1ea0f7621128b98009923551e10905648
Andrie Vorster <andrievorster@gmail.com>
2015-09-23 15:39:46 +0200

    some cleaning and fixing

--- setup.sh
+++ setup.sh
@@ -7,23 +7,24 @@ dotdirectory="$(dirname "$(readlink -f "$0")")"
 # Dotfiles:
 echo -e "Linking from:"
 echo -e "$dotdirectory"
-# X:
+# X...
 ln -fsv ${dotdirectory}/.xinitrc $HOME/.xinitrc
 ln -fsv ${dotdirectory}/.Xresources $HOME/.Xresources
-# i3:
+# i3...
 ln -fnsv ${dotdirectory}/.i3/ $HOME/.i3
-# Vim:
+# Vim...
 ln -fsv ${dotdirectory}/.vimrc $HOME/.vimrc
-# Git:
+# Git...
 ln -fsv ${dotdirectory}/.gitconfig $HOME/.gitconfig
-# Bash:
+# Bash...
 ln -fsv ${dotdirectory}/.bashrc $HOME/.bashrc
 ln -fsv ${dotdirectory}/.profile $HOME/.profile
-# !:
+# !...
 mkdir -p $HOME/bin/
-# Setup:
+# Setup...
 ln -fsv ${dotdirectory}/setup.sh $HOME/bin/mysetup
-# Pacman:
+# Pacman...
 ln -fsv ${dotdirectory}/pacman.sh $HOME/bin/mypacman
+
 # URxvt Font:
-if [ -s $HOME/.urxvt/ext/font-size ]; then echo -e "URxvt Font already exist..."; else curl -fLo $HOME/.urxvt/ext/font-size --create-dirs https://raw.githubusercontent.com/majutsushi/urxvt-font-size/master/font-size; fi
+if [ -f $HOME/.urxvt/ext/font-size ]; then echo -e "URxvt Font already exist..."; else curl -fLo $HOME/.urxvt/ext/font-size --create-dirs https://raw.githubusercontent.com/majutsushi/urxvt-font-size/master/font-size; fi
