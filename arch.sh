f3ce6f1e018d7a5c4d00b24cbc8248bd855bc5dd
Andrie Vorster <andrievorster@gmail.com>
2015-10-15 11:25:32 +0200

    changes

--- arch.sh
+++ arch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Setup {{{
+id=''
+[[ $EUID != 0 ]] && id='sudo'
+# }}}
 # Welcome {{{
 echo -e ""
 echo -e "Arch Linux Setup"
@@ -6,4 +10,33 @@ echo -e ""
 # }}}
 # Internet {{{
 ip link
+echo -n "Enter the WIFI device name: "
+read a_wip
+[[ -z "$a_wip" ]] && a_wip="!"
+case $a_wip in
+    [!]) echo -e "Skipping"
+        ;;
+    *) $id wifi-menu -o $a_wip
+        ;;
+esac
+echo -e ""
+ip link
+echo -n "Enter the DHCPCD device name: "
+read a_dip
+[[ -z "$a_dip" ]] && a_dip="!"
+case $a_dip in
+    [!]) echo -e "Skipping"
+        ;;
+    *) $id systemctl start dhcpcd@$a_dip.service
+        ;;
+esac
+echo -e ""
+# }}}
+# Time {{{
+timedatectl status
+echo -e ""
+timedatectl set-ntp true
+echo -e ""
+timedatectl status
+echo -e ""
 # }}}
